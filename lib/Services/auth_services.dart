8d5712d2a500c127a37d4af307ee2c6af3b3d11b
Tanti Wulansari <tantiwulansari2603@gmail.com>
2022-06-07 14:55:03 +0100

    tanti commit

---
+++ lib/Services/auth_services.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+
+import 'package:mobile_go_job/Services/globals.dart';
+import 'package:http/http.dart' as http;
+
+class AuthServices {
+  static Future<http.Response> register(
+      String nama_pelamar, String email, String password) async {
+    Map data = {
+      "nama_pelamar": nama_pelamar,
+      "email": email,
+      "password": password,
+    };
+    var body = json.encode(data);
+    var url = Uri.parse(baseURL);
+    http.Response response = await http.post(
+      url,
+      headers: headers,
+      body: body,
+    );
+    print(response.body);
+    return response;
+  }
+
+  static Future<http.Response> login(String email, String password) async {
+    Map data = {
+      "email": email,
+      "password": password,
+    };
+    var body = json.encode(data);
+    var url = Uri.parse(baseURL);
+    http.Response response = await http.post(
+      url,
+      headers: headers,
+      body: body,
+    );
+    print(response.body);
+    return response;
+  }
+}
---
+++ lib/Services/globals.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+const String baseURL = "http://127.0.0.1:8000/api/register"; //emulator localhost
+const Map<String, String> headers = {"Content-Type": "application/json"};
+
+errorSnackBar(BuildContext context, String text) {
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+    backgroundColor: Colors.red,
+    content: Text(text),
+    duration: const Duration(seconds: 1),
+  ));
+}
